9c2ae50f8466ea4d238ffde7b06cefc0a1141704
Lado Lomidze <lado.lomidze@gmail.com>
2015-08-06 18:35:14 +0400

    v0.0.1

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var gulpif = require('gulp-if');
+var sprity = require('sprity');
+
+gulp.task('default', function() {
+    return sprity.src({
+        src: './src/*.png',
+        orientation: 'binary-tree',
+        template: 'template/css.hbs',
+        prefix: 'sport',
+        name: 'sport-icons',
+        style: './sport-icons.css'
+    }).pipe(gulpif('*.png', gulp.dest('./dist/images/'), gulp.dest('./dist/css/')))
+});
